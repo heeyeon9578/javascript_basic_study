acfe151ca40669145d22d900dc356617c4f222ce
heeyeon9578 <heeyeon95781@gmail.com>
2024-02-16 16:58:07 +0900

    첫 커밋입니다.

---
+++ 1_basic/3_declaring_variable.js
@@ -0,0 +1,28 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ */
+var name = '코드팩토리';
+console.log(name);
+var age = 32;
+console.log(age);
+
+let ive='아이브';
+console.log(ive);
+
+/**
+ * let 과 var로 선언하면
+ * 값을 추후 변경할 수 있다, 
+ * 
+ */
+
+ive  = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+//newJeans = '안유진';
